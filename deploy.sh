0415ec1dc84e1a20c9fddc45b77c600d4ee0d26b
Yapcheekian <cheekian.yap@aiesec.net>
2020-01-01 18:26:31 +0800

    fix container name

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push yapcheekian/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=yapcheekian/multi-server:$SHA
-kubectl set image deployments/client-deployment web=yapcheekian/multi-client:$SHA
+kubectl set image deployments/client-deployment client=yapcheekian/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=yapcheekian/multi-worker:$SHA
