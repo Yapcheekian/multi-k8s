e5acad8308c7b911dc4a985c36bb4d46125585e0
Yapcheekian <cheekian.yap@aiesec.net>
2020-01-01 17:39:14 +0800

    2

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,14 @@ docker build -t yapcheekian/multi-server:latest -t yapcheekian/multi-server:$SHA
 docker build -t yapcheekian/multi-worker:latest -t yapcheekian/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push yapcheekian/multi-client:latest
-docker push yapcheekian/multi-client:$SHA
-
 docker push yapcheekian/multi-server:latest
-docker push yapcheekian/multi-server:$SHA
-
 docker push yapcheekian/multi-worker:latest
+
+docker push yapcheekian/multi-client:$SHA
+docker push yapcheekian/multi-server:$SHA
 docker push yapcheekian/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=yapcheekian/multi-server:$SHA
-kubectl set image deployments/client-deployment web=yapcheekian/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=yapcheekian/multi-worker:$SHA
+#kubectl set image deployments/server-deployment server=yapcheekian/multi-server:$SHA
+#kubectl set image deployments/client-deployment web=yapcheekian/multi-client:$SHA
+#kubectl set image deployments/worker-deployment worker=yapcheekian/multi-worker:$SHA
